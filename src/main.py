217df62e7ef63f7b0021c17f9cf2cab585319487
DominQu <dominik.kuczkowski00@gmail.com>
2021-05-07 12:22:06 +0200

    Setup up app window

--- src/main.py
+++ src/main.py
@@ -2,3 +2,15 @@
     function: create App class,
               combine functions from submodules
 '''
+from direct.showbase.ShowBase import ShowBase
+
+
+class App(ShowBase):
+
+    def __init__(self):
+
+        ShowBase.__init__(self)
+
+if __name__ == '__main__':
+    app = App()
+    app.run()
