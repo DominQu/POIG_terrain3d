79fb6d5ab8b970e6e969cf61e29d05942273dbab
DominQu <dominik.kuczkowski00@gmail.com>
2021-05-18 14:38:47 +0200

    Simplify _SetCamera method

--- src/main.py
+++ src/main.py
@@ -5,8 +5,9 @@
 from direct.showbase.ShowBase import ShowBase
 from direct.gui.DirectGui import *
 from panda3d.core import WindowProperties
+from panda3d.core import Vec3
 import sys
-sys.path.insert(0, '/home/dominik/GitDir/POIG_terrain3d/src')
+sys.path.insert(0, '/home/dominik/GitDir/POIG_terrain3d')
 from terrain import *
 
 
@@ -20,6 +21,58 @@ class App(ShowBase):
         self.ParamInit()
         self.MainMenu()
 
+        self.accept("w", self.UpdateKeymap, ["forward", True])
+        self.accept("w-up", self.UpdateKeymap, ["forward", False])
+        self.accept("s", self.UpdateKeymap, ["back", True])
+        self.accept("s-up", self.UpdateKeymap, ["back", False])
+        self.accept("a", self.UpdateKeymap, ["left", True])
+        self.accept("a-up", self.UpdateKeymap, ["left", False])
+        self.accept("d", self.UpdateKeymap, ["right", True])
+        self.accept("d-up", self.UpdateKeymap, ["right", False])
+        self.accept("q", self.UpdateKeymap, ["up", True])
+        self.accept("q-up", self.UpdateKeymap, ["up", False])
+        self.accept("e", self.UpdateKeymap, ["down", True])
+        self.accept("e-up", self.UpdateKeymap, ["down", False])
+        self.accept("arrow_left", self.UpdateKeymap, ["tiltleft", True])
+        self.accept("arrow_left-up", self.UpdateKeymap, ["tiltleft", False])
+        self.accept("arrow_right", self.UpdateKeymap, ["tiltright", True])
+        self.accept("arrow_right-up", self.UpdateKeymap, ["tiltright", False])
+        self.accept("arrow_up", self.UpdateKeymap, ["tiltup", True])
+        self.accept("arrow_up-up", self.UpdateKeymap, ["tiltup", False])
+        self.accept("arrow_down", self.UpdateKeymap, ["tiltdown", True])
+        self.accept("arrow_down-up", self.UpdateKeymap, ["tiltdown", False])
+
+        self.updateTask = taskMgr.add(self.update, "update")
+
+    def update(self, task):
+
+        dt = globalClock.getDt()
+
+        if self.keymap["forward"]:
+            self.camera.setPos(self.camera.getPos() + Vec3(0, 1000.0*dt, 0))
+        if self.keymap["back"]:
+            self.camera.setPos(self.camera.getPos() + Vec3(0, -1000.0*dt, 0))
+        if self.keymap["left"]:
+            self.camera.setPos(self.camera.getPos() + Vec3(-1000.0*dt, 0, 0))
+        if self.keymap["right"]:
+            self.camera.setPos(self.camera.getPos() + Vec3(1000.0*dt, 0, 0))
+        if self.keymap["up"]:
+            self.camera.setPos(self.camera.getPos() + Vec3(0, 0, 1000.0*dt))
+        if self.keymap["down"]:
+            self.camera.setPos(self.camera.getPos() + Vec3(0, 0, -1000.0*dt))
+        if self.keymap["tiltleft"]:
+            self.camera.setHpr(self.camera.getHpr() + Vec3(20*dt, 0, 0))
+        if self.keymap["tiltright"]:
+            self.camera.setHpr(self.camera.getHpr() + Vec3(-20*dt, 0, 0))
+        if self.keymap["tiltup"]:
+            self.camera.setHpr(self.camera.getHpr() + Vec3(0, -20*dt, 0))
+        if self.keymap["tiltdown"]:
+            self.camera.setHpr(self.camera.getHpr() + Vec3(0, 20*dt, 0))
+        return task.cont
+
+    def UpdateKeymap(self, direction, newstate):
+        self.keymap[direction] = newstate
+
     def ControlInit(self):
         self.disableMouse()
 
@@ -32,6 +85,36 @@ class App(ShowBase):
                              self.loader.loadTexture("UI/button_highlighted.png"),
                              self.loader.loadTexture("UI/button_disabled.png")
                             )
+        self.w = 2200
+        self.h = 1200
+        self.x = 200
+        self.y = 100
+        self.camerapos = {
+            "mountain": Vec3(600,-4000,3675),
+            "canyon":Vec3(500,-4500,2700)
+        }
+        self.camerahpr = {
+            "mountain": Vec3(-20,-20,0),
+            "canyon":Vec3(-21,-20,0)
+        }
+        self.texturefiles = {
+            "earth": 'textures/agri-small-autumn.jpg',
+            "moon": 'textures/agri-small-winter.jpg',
+            "river": 'textures/river.png',
+            "desert": 'textures/desert.png'
+        }
+        self.keymap = {
+            "forward": False,
+            "back": False,
+            "right": False,
+            "left": False,
+            "up": False,
+            "down": False,
+            "tiltleft": False,
+            "tiltright": False,
+            "tiltup": False,
+            "tiltdown": False
+        }
         
     def MainMenu(self):
 
@@ -102,7 +185,7 @@ class App(ShowBase):
 
         button1 = DirectButton(text="Ziemia",
                                command = self.Light,
-                               extraArgs = ["mountain", "ziemia", self.mountainmenu],
+                               extraArgs = ["mountain", "earth", self.mountainmenu],
                                pos = (0,0,0.1),
                                parent = self.mountainmenu,
                                scale = 0.1,
@@ -116,7 +199,7 @@ class App(ShowBase):
 
         button2 = DirectButton(text="Księżyc",
                                command= self.Light,
-                               extraArgs = ["mountain", "ksiezyc", self.mountainmenu],
+                               extraArgs = ["mountain", "moon", self.mountainmenu],
                                pos = (0,0,-0.2),
                                parent = self.mountainmenu,
                                scale = 0.1,
@@ -144,7 +227,7 @@ class App(ShowBase):
 
         button1 = DirectButton(text="Rzeka",
                                command = self.Light,
-                               extraArgs = ["canyon", "rzeka", self.canyonmenu],
+                               extraArgs = ["canyon", "river", self.canyonmenu],
                                pos = (0,0,0.1),
                                parent = self.canyonmenu,
                                scale = 0.1,
@@ -158,7 +241,7 @@ class App(ShowBase):
 
         button2 = DirectButton(text="Pustynia",
                                command= self.Light,
-                               extraArgs = ["canyon", "pustynia", self.canyonmenu],
+                               extraArgs = ["canyon", "desert", self.canyonmenu],
                                pos = (0,0,-0.2),
                                parent = self.canyonmenu,
                                scale = 0.1,
@@ -173,31 +256,48 @@ class App(ShowBase):
     def Light(self, terrain, texture, submenu):
         self.StartApp(terrain, texture, submenu)
 
-    def StartApp(self, terrain, texture, submenu):
+    def StartApp(self, terraintype, texture, submenu):
+        
         submenu.hide()
         self.titleMenuBackdrop.hide()
 
-        w, h = 1800, 1200
-
-        props = WindowProperties()
-        props.setSize(w,h)
-        base.win.requestProperties(props)
+        self.props = WindowProperties()
+        self.props.setSize(self.w,self.h)
+        self.props.setOrigin(self.x, self.y)
+        base.win.requestProperties(self.props)
         
-        terrain = Terrain(self.render)
-        terrain_node = terrain.create_terrain()
+        self.terrain = Terrain(self.render, terraintype)
+        terrain_node = self.terrain.create_terrain()
         self.terrainNodePath = self.render.attachNewNode(terrain_node)
 
-        self.SetCamera()
-        self.SetTexture()
-        self.SetLights()
+        self._SetCamera(terraintype)
+        self._SetTexture(texture)
+        self._SetLights()
 
-    def SetCamera(self):
-        pass
+    def _SetCamera(self, terraintype):
 
-    def SetTexture(self):
-        pass
+        # if terraintype == "mountain":
+        #     self.camera.setPos(600,-4000,self.terrain.camera + 2500)
+        #     self.camera.setHpr(-20,-20,0)
+        #     print(self.terrain.camera)
+        # elif terraintype == "canyon":
+        #     self.camera.setPos(500,-4500,self.terrain.camera + 2500)
+        #     self.camera.setHpr(-21,-20,0)
+        #     print(self.terrain.camera)
 
-    def SetLights(self):
+        self.camera.setPos(self.camerapos[terraintype])
+        self.camera.setHpr(self.camerahpr[terraintype])
+
+
+        
+
+    def _SetTexture(self, texture):
+        
+        ts = TextureStage('ts')
+        tx = self.loader.loadTexture(self.texturefiles[texture])
+        self.terrainNodePath.setTexture(ts, tx)
+
+    def _SetLights(self):
         pass
 
 
